74165401bdb452582f6d4c91e39b62d1423a7e5e
Rimantas <belauzas@yahoo.com>
2022-03-30 11:37:18 +0300

    Strings

---
+++ js/03-string.js
@@ -0,0 +1,34 @@
+/*
+STRING
+
+kabutes:
+- viengubos (')
+- dvygubos (")
+*/
+
+const name = 'Petras';
+console.log(name);
+
+const lastname = "Petraitis";
+console.log(lastname);
+
+const viegubaKabute = "viengubos (')";
+console.log(viegubaKabute);
+
+const dvygubaKabute = 'dvygubos (")';
+console.log(dvygubaKabute);
+
+const viegubaKabute12 = 'Cia yra vienguba (\') ir dvyguba (") kabutes.';
+console.log(viegubaKabute12);
+
+const dvygubaKabute12 = "Cia yra vienguba (') ir dvyguba (\") kabutes.";
+console.log(dvygubaKabute12);
+
+const kabute12 = "Cia yra vienguba (\') ir dvyguba (\") kabutes.";
+console.log(kabute12);
+
+const clientName = 'Maryte';
+const clientAge = 87;
+// Client Maryte is 87 years old.
+const clientSentence = 'Client ' + clientName + ' is ' + clientAge + ' years old.';
+console.log(clientSentence);
