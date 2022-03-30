6a42decb73782914954cb806eba4d9dab0f49144
Rimantas <belauzas@yahoo.com>
2022-03-30 13:10:31 +0300

    If

---
+++ js/05-if.js
@@ -0,0 +1,21 @@
+/*
+IF
+
+Sablonai:
+- if () {}
+- if () {} else {}
+
+Palyginimo operatoriai:
+- visi: >, <, <=, >=, ==, !=, ===, !==
+- naudotini: >, <, <=, >=, ===, !==
+- NEnaudotini: ==, !=
+*/
+
+const a = 7;
+const b = '5';
+
+if (a !== b) {
+    console.log('A yra lygu B');
+} else {
+    console.log('A nera lygu B');
+}
