ac1e5af8a1fd167d4a76b01bf89097f1c1755878
Rimantas <belauzas@yahoo.com>
2022-03-30 11:09:23 +0300

    Numbers

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -6,6 +6,11 @@ NUMBERS:
 - nenormalus:
     - NaN (not-a-number)
     - Infinity; -Infinity
+
+OPERATORIAI:
+++ - padidinimas vienu vienetu
+-- - sumazinimas vienu vienetu
++=, -=, /=, *=, %= - savo paties reiksmes pakeitimas (a = a + 5 --> a += 5)
 */
 
 const sveikasis = 5;
@@ -85,3 +90,87 @@ console.log(false + 7);
 console.log(false - 7);
 console.log(false * 7);
 console.log(false / 7);
+
+console.log('-----------');
+console.log(2 ** 4);
+console.log(3.14 ** 3.14);
+console.log(7 % 2);
+console.log(13 % 5);
+console.log(13 % 14);
+
+console.log('-----------');
+let pinigineAfter = 0;
+console.log('Pinigine:', pinigineAfter++);
+console.log('Pinigine:', pinigineAfter++);
+console.log('Pinigine:', pinigineAfter++);
+console.log('Pinigine:', pinigineAfter++);
+console.log('Pinigine:', pinigineAfter++);
+
+console.log('-----------');
+let pinigineBefore = 0;
+console.log('Pinigine:', ++pinigineBefore);
+console.log('Pinigine:', ++pinigineBefore);
+console.log('Pinigine:', ++pinigineBefore);
+console.log('Pinigine:', ++pinigineBefore);
+console.log('Pinigine:', ++pinigineBefore);
+
+console.log('-----------');
+let skolaAfter = 0;
+console.log('Skola:', skolaAfter--);
+console.log('Skola:', skolaAfter--);
+console.log('Skola:', skolaAfter--);
+console.log('Skola:', skolaAfter--);
+console.log('Skola:', skolaAfter--);
+
+console.log('-----------');
+let skolaBefore = 0;
+console.log('Skola:', --skolaBefore);
+console.log('Skola:', --skolaBefore);
+console.log('Skola:', --skolaBefore);
+console.log('Skola:', --skolaBefore);
+console.log('Skola:', --skolaBefore);
+
+console.log('-----------');
+let a = 0;
+console.log(a);
+a = a + 1;
+console.log(a);
+a++;
+console.log(a);
+++a;
+console.log(a);
+
+console.log('-----------');
+let b = 0;
+console.log(b);
+b = b + 3;
+console.log(b);
+b++;
+b++;
+b++;
+console.log(b);
+b += 3;
+console.log(b);
+b += 3;
+console.log(b);
+b += 3;
+console.log(b);
+
+console.log('-----------');
+let c = 16;
+const d = 2;
+console.log(c);
+c = c / d;
+console.log(c);
+c /= d;
+console.log(c);
+c /= d;
+console.log(c);
+c /= d;
+console.log(c);
+
+console.log('-----------');
+let e = 5;
+console.log(e);
+e %= e;
+console.log(e);
