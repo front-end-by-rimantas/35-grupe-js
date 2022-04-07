6ebdab0f6f01e6329e5d23a91e45b9adf41c671f
Rimantas <belauzas@yahoo.com>
2022-04-07 13:17:39 +0300

    Final

--- js/18-method.js
+++ js/18-method.js
@@ -175,7 +175,174 @@ console.log('extra');
 console.log(rytas.reverse());
 
 // map, filter, sort, reduce, every, flat, forEach
+const marks = [10, 2, 8, 4, 6];
+console.log('map - individualiu reiksmiu konvertavimas');
+console.log(marks.map(item => item));
+console.log(marks.map(item => 2 * item));
+console.log(marks.map(item => item % 3));
+console.log(marks.map(item => item - 0.5));
+
+console.log('filter');
+console.log(marks.filter(item => item));
+console.log(marks.filter(item => item > 5));
+console.log(marks.filter(item => item <= 5));
+console.log(marks.filter(item => item > 5 && item % 3 === 0));
+
+console.log('reduce - sutraukia visas reiksmes i viena reiksme');
+console.log(marks.reduce((total, item) => total + item, 0));
+console.log(marks.reduce((total, item) => total + item));
+console.log(marks.reduce((total, item) => total * item, 1));
+console.log(marks.reduce((total, item) => total * item));
+console.log(marks.reduce((total, item) => total - item, 0));
+console.log(marks.reduce((total, item) => total - item));
+console.log(marks.reduce((total, item) => total / item, 0));
+console.log(marks.reduce((total, item) => total / item));
+
+console.log('sort');
+const nums = [1, 2, 3, 10, 20, 30, 11, 21, 31];
+console.log(marks.sort());
+console.log(nums.sort());
+console.log(nums.sort((a, b) => a - b));
+console.log(nums.sort((a, b) => b - a));
+
+const texts = ['asd', 'qw', 'asdas', 's'];
+console.log(texts.sort());
+console.log(texts.sort().reverse());
+console.log(texts.sort((a, b) => a.length - b.length));
+console.log(texts.sort((a, b) => b.length - a.length));
+
+console.log('flat');
+const matrix = [
+    [1, 2, [7, 8]],
+    [11, [9, 10], 22],
+    [[11, 12], 3, 4],
+];
+console.log(matrix.flat());
+console.log(matrix.flat(2));
+
+console.log('... - spread operator');
+console.log(Math.min(1, 5, 7, -4, 10));
+console.log(Math.max(1, 5, 7, -4, 10));
+const minMax = [1, 5, 7, -4, 10];
+console.log(Math.min(...minMax));
+console.log(Math.max(...minMax));
+
+const m1 = [1, 2];
+const m2 = [3, 4];
+const m3 = [5, 6];
+const m123 = [...m1, 123, ...m2, ...m3];
+const m132 = [...m1, ...m3, 132, ...m2];
+const m321 = [...m3, ...m2, ...m1, 321];
+console.log(m123);
+console.log(m132);
+console.log(m321);
 
 /*
 OBJECT
 */
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+
+const person = {
+    name: 'Petras',
+    age: 99,
+    isMarried: true
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.isMarried);
+console.log(person['name']);
+console.log(person['age']);
+console.log(person['isMarried']);
+
+console.log('automatinis visu objekto raktazodziu istraukimas');
+const personKeys = Object.keys(person);
+console.log(personKeys);
+
+console.log('automatinis visu objekto reiksmiu istraukimas');
+for (const key of personKeys) {
+    console.log(key, ':', person[key]);
+}
+
+console.log('FOR-IN');
+for (const key in person) {
+    console.log(key, ':', person[key]);
+}
+
+console.log('... - spread operator (naudojimas objekte)');
+console.log({ ...person });
+console.log({ ...person, car: 'Tesla' });
+console.log({ color: 'red', ...person });
+console.log({ ...person, isMarried: false });
+console.log({ isMarried: false, ...person });
+
+const objA = {
+    a: 'a',
+    c: 'c',
+    b: 'bbbbbb'
+}
+const objB = {
+    b: 'b',
+    d: 'd'
+}
+
+const objAB = { ...objA, ...objB };
+const objBA = { ...objB, ...objA };
+console.log(objAB);
+console.log(objBA);
+
+/*
+DESTRUCTURING - destrukturizavimas
+*/
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+
+const skaiciai = [10, 2, 8, 6, 4, 2, 8, 6, 4];
+const pirmasSkaicius = skaiciai[0];
+const antrasSkaicius = skaiciai[1];
+const treciasSkaicius = skaiciai[2];
+console.log(pirmasSkaicius, antrasSkaicius, treciasSkaicius);
+
+const [pirmas, antras, trecias, ...kita] = skaiciai;
+console.log(pirmas, antras, trecias, kita);
+
+const skaiciai2 = [9999]
+const [xx1, ...xxx] = skaiciai2;
+console.log(xx1, xxx);
+
+const car = {
+    name: 'Tesla',
+    model: 'X',
+    color: 'red'
+}
+const carName = car.name;
+const carModel = car.model;
+const carColor = car.color;
+console.log(carName, carModel, carColor);
+
+const { name, color, model } = car;
+console.log(name, color, model);
+
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+console.log('--------------');
+
+const random = Math.floor(Math.random() * 10);  // [0..9]
+console.log(random);
+
+function randomNumber(nuo = 0, iki = 9) {
+    return nuo + Math.floor(Math.random() * (iki - nuo + 1));
+}
+
+for (let i = 0; i < 20; i++) {
+    console.log(randomNumber(0, 10));
+}
